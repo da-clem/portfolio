94fe3cc564bc51953d324c45e9916a53d5161520
clement lattelais <clement.wt@gmail.com>
2019-11-28 16:28:03 +0000

    add pages

--- .eleventy.js
+++ .eleventy.js
@@ -5,11 +5,12 @@ module.exports = function(eleventyConfig) {
   return {
     passthroughFileCopy: true,
     markdownTemplateEngine: "njk",
-    templateFormats: ["html", "njk", "md"]
+    templateFormats: ["html", "njk", "md"],
+    
     dir: {
       input: "src",
       output: "_site",
-      include: "includes"
+      includes: "includes"
     }
   }
 
