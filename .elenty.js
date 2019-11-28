85480aee5de22a97f3b2d546c763983223a698b1
clement lattelais <clement.wt@gmail.com>
2019-11-28 10:57:09 +0000

    project structure

---
+++ .elenty.js
@@ -0,0 +1,16 @@
+module.exports = function(eleventyConfig) {
+  
+  eleventyConfig.addPassthroughCopy("assets");
+
+  return {
+
+    dir: {
+      input: "src",
+      output: "_site",
+      include: "includes"
+    }
+  }
+
+
+
+}
